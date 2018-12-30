7e87590204859aa07eded7f1a352d56a13f5da90
Nguyễn Minh Tuấn <minhtuan@eye-solution.vn>
2018-12-30 10:17:40 +0700

    test

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 // Outline
 var html = '';
-console.log('aaaa');
+
 $('.ds-main .field--type-text-with-summary').find('h2').each(function (i, e) {
     $(this).attr('id', i);
     var g = $(this).html();
     html += `<div class="scroll-animated" style="font-size: 12px;" rel="#${i}">${g}</div>`;
 })
-
+console.log(html);
 $('.ad-300').html(html);
 
 $(".scroll-animated").click(function () {
