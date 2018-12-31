a67ac22624181f3afeefedb03dbb1bd22c150bad
Nguyen Tien Doanh <ntdoanhce@gmail.com>
2018-12-31 18:00:41 +0700

    fix css artile

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ var html = '';
 $('.ds-main .field--type-text-with-summary').find('h2').each(function (i, e) {
     $(this).attr('id', i);
     var g = $(this).html();
-    html += `<div class="scroll-animated" style="font-size: 12px;" rel="#${i}">${g}</div>`;
+    html += `<div class="scroll-animated" rel="#${i}">${g}</div>`;
 })
 
 $('.ad-299').html(html);
