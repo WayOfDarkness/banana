49909b4e4116e73036dc94837d09badd7c915a13
Nguyễn Minh Tuấn <minhtuan@eye-solution.vn>
2018-12-31 14:39:25 +0700

    login-register

---
+++ js/login-register.js
@@ -0,0 +1,149 @@
+jQuery(document).ready(function($){
+	var $form_modal = $('.cd-user-modal'),
+		$form_login = $form_modal.find('#cd-login'),
+		$form_signup = $form_modal.find('#cd-signup'),
+		$form_forgot_password = $form_modal.find('#cd-reset-password'),
+		$form_modal_tab = $('.cd-switcher'),
+		$tab_login = $form_modal_tab.children('li').eq(0).children('a'),
+		$tab_signup = $form_modal_tab.children('li').eq(1).children('a'),
+		$forgot_password_link = $form_login.find('.cd-form-bottom-message a'),
+		$back_to_login_link = $form_forgot_password.find('.cd-form-bottom-message a'),
+		$main_nav = $('.main-nav');
+
+	//open modal
+	$main_nav.on('click', function(event){
+
+		if( $(event.target).is($main_nav) ) {
+			// on mobile open the submenu
+			$(this).children('ul').toggleClass('is-visible');
+		} else {
+			// on mobile close submenu
+			$main_nav.children('ul').removeClass('is-visible');
+			//show modal layer
+			$form_modal.addClass('is-visible');	
+			//show the selected form
+			( $(event.target).is('.cd-signup') ) ? signup_selected() : login_selected();
+		}
+
+	});
+
+	$('.cd-signin', '.cd-signup').click(function(e) {
+		$('.header-icon.user-button').removeClass('close');
+		$('#account-dropdown').hide();
+	});
+
+	//close modal
+	$('.cd-user-modal').on('click', function(event){
+		if( $(event.target).is($form_modal) || $(event.target).is('.cd-close-form') ) {
+			$form_modal.removeClass('is-visible');
+		}	
+	});
+	//close modal when clicking the esc keyboard button
+	$(document).keyup(function(event){
+    	if(event.which=='27'){
+    		$form_modal.removeClass('is-visible');
+	    }
+    });
+
+	//switch from a tab to another
+	$form_modal_tab.on('click', function(event) {
+		event.preventDefault();
+		( $(event.target).is( $tab_login ) ) ? login_selected() : signup_selected();
+	});
+
+	//hide or show password
+	$('.hide-password').on('click', function(){
+		var $this= $(this),
+			$password_field = $this.prev('input');
+		( 'password' == $password_field.attr('type') ) ? $password_field.attr('type', 'text') : $password_field.attr('type', 'password');
+		$password_field.putCursorAtEnd();
+	});
+
+	$('.cd-form').on('click', '.hide-password-on', function(){
+		$(this).removeClass('hide-password-on');
+		$(this).addClass('hide-password-off');
+	});
+
+	$('.cd-form').on('click', '.hide-password-off', function(){
+		$(this).removeClass('hide-password-off');
+		$(this).addClass('hide-password-on');
+	});
+
+	//show forgot-password form 
+	$forgot_password_link.on('click', function(event){
+		event.preventDefault();
+		forgot_password_selected();
+	});
+
+	//back to login from the forgot-password form
+	$back_to_login_link.on('click', function(event){
+		event.preventDefault();
+		login_selected();
+	});
+
+	function login_selected(){
+		$form_login.addClass('is-selected');
+		$form_signup.removeClass('is-selected');
+		$form_forgot_password.removeClass('is-selected');
+		$tab_login.addClass('selected');
+		$tab_signup.removeClass('selected');
+	}
+
+	function signup_selected(){
+		$form_login.removeClass('is-selected');
+		$form_signup.addClass('is-selected');
+		$form_forgot_password.removeClass('is-selected');
+		$tab_login.removeClass('selected');
+		$tab_signup.addClass('selected');
+	}
+
+	function forgot_password_selected(){
+		$form_login.removeClass('is-selected');
+		$form_signup.removeClass('is-selected');
+		$form_forgot_password.addClass('is-selected');
+	}
+
+	$form_login.find('input[type="submit"]').on('click', function(event){
+		event.preventDefault();
+		$form_login.find('input[type="email"]').toggleClass('has-error').next('span').toggleClass('is-visible');
+	});
+	$form_signup.find('input[type="submit"]').on('click', function(event){
+		event.preventDefault();
+		$form_signup.find('input[type="email"]').toggleClass('has-error').next('span').toggleClass('is-visible');
+	});
+
+
+	if(!Modernizr.input.placeholder){
+		$('[placeholder]').focus(function() {
+			var input = $(this);
+			if (input.val() == input.attr('placeholder')) {
+				input.val('');
+		  	}
+		}).blur(function() {
+		 	var input = $(this);
+		  	if (input.val() == '' || input.val() == input.attr('placeholder')) {
+				input.val(input.attr('placeholder'));
+		  	}
+		}).blur();
+		$('[placeholder]').parents('form').submit(function() {
+		  	$(this).find('[placeholder]').each(function() {
+				var input = $(this);
+				if (input.val() == input.attr('placeholder')) {
+			 		input.val('');
+				}
+		  	})
+		});
+	}
+
+});
+
+jQuery.fn.putCursorAtEnd = function() {
+	return this.each(function() {
+    	if (this.setSelectionRange) {
+      		var len = $(this).val().length * 2;
+      		this.setSelectionRange(len, len);
+    	} else {
+      		$(this).val($(this).val());
+    	}
+	});
+};
