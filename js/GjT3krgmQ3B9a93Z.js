4fdd496af05147b6359981424988c9bcfc1a31d1
WingsOfPegasuS <mw.goldensword@gmail.com>
2019-04-27 21:03:07 +0700

    Update GjT3krgmQ3B9a93Z.js

--- js/GjT3krgmQ3B9a93Z.js
+++ js/GjT3krgmQ3B9a93Z.js
@@ -1,10 +1,20 @@
-$(document).ready(function () {
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
+ 
+ $(document).ready(function () {
     var r1 = 10;
     var r2 = 13;
     var r3 = 5;
     var html = '';
     for (var i = 1; i < 101; i++) {
-        html += `<a href="${i == r1 ? '/abc' : (i == r2 ? '/def' : (i == r3 ? '/xyz' : '#'))}">${i}</a>${i % 10 == 0 ? '<br>' : ''}`;
+        html += `<a href="${i == r1 ? '/abc' : (i == r2 ? '/def' : (i == r3 ? '/xyz' : '/' + makeid(10)))}">${i}</a>${i % 10 == 0 ? '<br>' : ''}`;
     }
     $('.one-hundred-doors').html(html);
 });
