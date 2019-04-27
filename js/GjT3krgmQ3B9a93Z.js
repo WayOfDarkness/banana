84b1cad7e0daba8166f7a1a3be20cbb7f30ed46f
WingsOfPegasuS <mw.goldensword@gmail.com>
2019-04-27 20:32:43 +0700

    Update GjT3krgmQ3B9a93Z.js

--- js/GjT3krgmQ3B9a93Z.js
+++ js/GjT3krgmQ3B9a93Z.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
     var r1 = 10;
     var r2 = 13;
     var r3 = 5;
-    var html;
-    for (i = 1; i < 1001; i++) {
+    html;
+    for (var i = 1; i < 1001; i++) {
         html += `<a href="${i == r1 ? '/abc' : (i == r2 ? '/def' : (i == r3 ? '/xyz' : '#'))}">${i}</a>`;
     }
     $('.one-hundred-doors').html(html);
