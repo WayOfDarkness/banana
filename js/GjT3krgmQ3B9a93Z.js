fd56cadb86048111646d5cd653e6e96f0fc963a5
WingsOfPegasuS <mw.goldensword@gmail.com>
2019-04-27 20:56:27 +0700

    vg

--- js/GjT3krgmQ3B9a93Z.js
+++ js/GjT3krgmQ3B9a93Z.js
@@ -3,8 +3,8 @@ $(document).ready(function () {
     var r2 = 13;
     var r3 = 5;
     var html = '';
-    for (var i = 1; i < 1001; i++) {
-        html += `<a href="${i == r1 ? '/abc' : (i == r2 ? '/def' : (i == r3 ? '/xyz' : '#'))}">${i}</a>`;
+    for (var i = 1; i < 101; i++) {
+        html += `<a href="${i == r1 ? '/abc' : (i == r2 ? '/def' : (i == r3 ? '/xyz' : '#'))}">${i}</a>${i == 10 ? '<br>' : ''}`;
     }
     $('.one-hundred-doors').html(html);
 });
