4fba29c71d0cc7c84bc8a63a8771ea81e3fe0c84
WingsOfPegasuS <mw.goldensword@gmail.com>
2019-04-27 20:36:04 +0700

    Update GjT3krgmQ3B9a93Z.js

--- js/GjT3krgmQ3B9a93Z.js
+++ js/GjT3krgmQ3B9a93Z.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     var r1 = 10;
     var r2 = 13;
     var r3 = 5;
-    var html;
+    var html = '';
     for (var i = 1; i < 1001; i++) {
         html += `<a href="${i == r1 ? '/abc' : (i == r2 ? '/def' : (i == r3 ? '/xyz' : '#'))}">${i}</a>`;
     }
