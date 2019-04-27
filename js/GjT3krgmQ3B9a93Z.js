d86e66d6dbded3bb22fa18004fa65dfb92ae6253
WingsOfPegasuS <mw.goldensword@gmail.com>
2019-04-27 21:39:04 +0700

    Update GjT3krgmQ3B9a93Z.js

--- js/GjT3krgmQ3B9a93Z.js
+++ js/GjT3krgmQ3B9a93Z.js
@@ -1,12 +1,12 @@
 var counter = 0;
 function makeid(param) {
     if (param != 'none') {
-        window.location.href = `/${param}`;
-    } else {
-        counter++;
-        console.log('clicked!');
-        if (counter > 10) window.location.reload();
-    }
+        window.location.href = `/${param}`;}
+    // } else {
+    //     counter++;
+    //     console.log('clicked!');
+    //     if (counter > 10) window.location.reload();
+    // }
  }
 
  $(document).ready(function () {
@@ -15,7 +15,7 @@ function makeid(param) {
     var r3 = Math.floor((Math.random() * 100) + 1);
     var html = '';
     for (var i = 1; i < 101; i++) {
-        html += `<button onClick="${i == r1 ? makeid('Nuf8cqH02k') : (i == r2 ? makeid('JzusnaVVXX') : (i == r3 ?  makeid('6uCqm1Sl2B') : makeid('none')))}">${i}</button>${i % 10 == 0 ? '<br>' : ''}`;
+        html += `<button onClick="${i == r1 ? makeid('Nuf8cqH02k') : (i == r2 ? makeid('JzusnaVVXX') : (i == r3 ? makeid('6uCqm1Sl2B') : makeid('none')))}">${i}</button>${i % 10 == 0 ? '<br>' : ''}`;
     }
     $('.one-hundred-switches').html(html);
 });
