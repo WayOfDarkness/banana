f0c5ac84a70364339d13ecda66bcf1673d62b5bd
WingsOfPegasuS <mw.goldensword@gmail.com>
2019-04-27 21:07:39 +0700

    fg

--- js/GjT3krgmQ3B9a93Z.js
+++ js/GjT3krgmQ3B9a93Z.js
@@ -9,12 +9,13 @@ function makeid(length) {
  }
  
  $(document).ready(function () {
-    var r1 = 10;
-    var r2 = 13;
-    var r3 = 5;
+    var r1 = Math.floor(Math.random() * 1000);
+    var r2 = Math.floor(Math.random() * 1000);
+    var r3 = Math.floor(Math.random() * 1000);
+    console.log(r1 + " " + r2 + " " + r3);
     var html = '';
     for (var i = 1; i < 101; i++) {
-        html += `<a href="${i == r1 ? '/abc' : (i == r2 ? '/def' : (i == r3 ? '/xyz' : '/' + makeid(10)))}">${i}</a>${i % 10 == 0 ? '<br>' : ''}`;
+        html += `<a href="${i == r1 ? '/Nuf8cqH02k' : (i == r2 ? '/JzusnaVVXX' : (i == r3 ? '/6uCqm1Sl2B' : '/' + makeid(10)))}">${i}</a>${i % 10 == 0 ? '<br>' : ''}`;
     }
-    $('.one-hundred-doors').html(html);
+    $('.one-hundred-switches').html(html);
 });
