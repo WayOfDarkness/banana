fa551baedfdedc3612f05f2f13531bcc00e66c6d
WingsOfPegasuS <mw.goldensword@gmail.com>
2019-05-01 18:19:04 +0700

    á

--- js/event.js
+++ js/event.js
@@ -27,6 +27,15 @@
 // }
 
 $(document).ready(function () {
+	$('.button-group .button').click (function() {
+		var btnID = $(this).attr('id');
+		if (btnID == 'start-btn')
+			window.location.href = '/all-riddles';
+		else if (btnID = 'ranking-btn')
+			window.location.href = '/bang-xep-hang';
+		else $(btnID).show();
+	});
+
 	$('.close-btn').click(function () {
 		$(this).closest('.modal').hide();
 	})
