77c509a7594ed88fd0716c702405f7fda18e423b
WingsOfPegasuS <mw.goldensword@gmail.com>
2019-05-01 18:11:06 +0700

    Update event.js

--- js/event.js
+++ js/event.js
@@ -1,30 +1,33 @@
-const body = document.body;
-const btn = document.querySelectorAll('.button')[0];
+// const body = document.body;
+// const btn = document.querySelectorAll('.button')[0];
 
-btn.addEventListener('mouseenter', () => {
-	body.classList.add('show');
+// btn.addEventListener('mouseenter', () => {
+// 	body.classList.add('show');
+// });
+
+// btn.addEventListener('mouseleave', () => {
+// 	body.classList.remove('show');
+// });
+
+// function clickFunc(clicked_id) {
+// 	// console.info(clicked_id);
+// 	id_close_modal = "close" + clicked_id;
+// 	var span = document.getElementById(id_close_modal);
+// 	var name = 'modal' + clicked_id;
+// 	var modal = document.getElementById(name);
+// 	modal.style.display = "block";
+// 	window.onclick = function (event) {
+// 		if (event.target == modal) {
+// 			modal.style.display = "none";
+// 		}
+// 	}
+// 	span.onclick = function () {
+// 		modal.style.display = "none";
+// 	}
+// }
+
+$(document).ready(function () {
+	$('.close-btn').click(function () {
+		$(this).parent('.modal').hide();
+	})
 });
-
-btn.addEventListener('mouseleave', () => {
-	body.classList.remove('show');
-});
-
-
-
-function clickFunc(clicked_id){
-	// console.info(clicked_id);
-	id_close_modal = "close" + clicked_id;
-	var span = document.getElementById(id_close_modal); 
-	var name = 'modal'+ clicked_id;
-	var modal = document.getElementById(name);
-	modal.style.display = "block";  
- 	 window.onclick = function(event) {
- 			 if (event.target == modal) {
-   			 modal.style.display = "none";
-  				}
-			}
-	span.onclick = function() {
- 	modal.style.display = "none";
- 	}
-}
-
