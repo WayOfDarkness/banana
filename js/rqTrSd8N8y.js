ff02c1278f64b5c7a674fe260546f3554fe81fa2
WingsOfPegasuS <mw.goldensword@gmail.com>
2019-05-04 13:57:02 +0700

    gh

--- js/rqTrSd8N8y.js
+++ js/rqTrSd8N8y.js
@@ -1,8 +1,15 @@
 $(document).ready(function () {
+    var currentNumber;
     setInterval(function(){ 
-        var currentNumber = parseInt($('#counter-platform').html());
+        currentNumber = parseInt($('#counter-platform').html());
         if (currentNumber > 0) {
             $('#counter-platform').html(currentNumber - 1);
         }
     }, 1000);
+
+    $('.red').click(function() {
+        if (currentNumber == 78 || currentNumber == 73 || currentNumber == 70 || currentNumber == 50 || currentNumber == 35 || currentNumber == 17) {
+            $('#counter-platform').html(99);
+        }
+    })
 });
