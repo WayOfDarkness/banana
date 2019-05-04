99e2d1e76dfdc7831409f30635a30c1b5f934fe9
WingsOfPegasuS <mw.goldensword@gmail.com>
2019-05-04 14:06:11 +0700

    Update rqTrSd8N8y.js

--- js/rqTrSd8N8y.js
+++ js/rqTrSd8N8y.js
@@ -8,8 +8,41 @@ $(document).ready(function () {
     }, 1000);
 
     $('.red').click(function() {
-        if (currentNumber == 79 || currentNumber == 74 || currentNumber == 71 || currentNumber == 51 || currentNumber == 36 || currentNumber == 18) {
+        if ($(this).hasClass('active')) {
+            if (currentNumber == 79 || currentNumber == 74 || currentNumber == 71 || currentNumber == 51 || currentNumber == 36 || currentNumber == 18) {
+                $('#counter-platform').html(99);
+            }
+        } else {
             $('#counter-platform').html(99);
+            $('.yellow').removeClass('active');
+            $('.green').removeClass('active');
+            $(this).addClass('active');
+        }
+    })
+
+    $('.yellow').click(function() {
+        if ($(this).hasClass('active')) {
+            if (currentNumber == 17 || currentNumber == 11) {
+                $('#counter-platform').html(30);
+            }
+        } else {
+            $('#counter-platform').html(30);
+            $('.red').removeClass('active');
+            $('.green').removeClass('active');
+            $(this).addClass('active');
+        }
+    })
+
+    $('.green').click(function() {
+        if ($(this).hasClass('active')) {
+            if (currentNumber == 45 || currentNumber == 36 || currentNumber == 29 || currentNumber == 21 || currentNumber == 1) {
+                $('#counter-platform').html(57);
+            }
+        } else {
+            $('#counter-platform').html(57);
+            $('.yellow').removeClass('active');
+            $('.red').removeClass('active');
+            $(this).addClass('active');
         }
     })
 });
