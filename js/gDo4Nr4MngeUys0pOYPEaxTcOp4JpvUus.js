4cc47872aaf0bcd27086ac81ec25953be84b880d
WingsOfPegasuS <mw.goldensword@gmail.com>
2019-05-04 09:55:53 +0700

    Update gDo4Nr4MngeUys0pOYPEaxTcOp4JpvUus.js

--- js/gDo4Nr4MngeUys0pOYPEaxTcOp4JpvUus.js
+++ js/gDo4Nr4MngeUys0pOYPEaxTcOp4JpvUus.js
@@ -47,7 +47,7 @@ $(document).ready(function () {
 
     if (customer_id && point == 0) {
 
-        // Change The Domino Dance Riddle Status From Accessible To Solved
+        // Change The Current Riddle Status From Accessible To Solved
         for (var i = 0; i < set_role_to_1.split(' ').length; i++) {
             $.ajax({
                 type: 'POST',
@@ -61,71 +61,15 @@ $(document).ready(function () {
             });
         }
         
-        // Change The Four Paths Status From Unavailable To Accessible
-        $.ajax({
-            type: 'POST',
-            url: '/api/setRole',
-            data: {
-                'gallery_id': 1,
-                'customer_id': customer_id,
-                'role': 1
-            },
-            success: function (result) { }
-        });
-
-        // Change Spring 1 Riddle Status From Unavailable To Accessible
-        for (var i = 14; i <= 17; i = i + 3) {
+        // Change The Next Riddles Status From Unavailable To Accessible
+        for (var i = 0; i < set_role_to_2.split(' ').length; i++) {
             $.ajax({
                 type: 'POST',
                 url: '/api/setRole',
                 data: {
-                    'gallery_id': i,
+                    'gallery_id': set_role_to_2.split(' ')[i],
                     'customer_id': customer_id,
-                    'role': 1
-                },
-                success: function (result) { }
-            });
-        }
-
-        // Change Summer 1 Riddle Status From Unavailable To Accessible
-        for (var i = 54; i <= 63; i++) {
-            if (i != 59) {
-                $.ajax({
-                    type: 'POST',
-                    url: '/api/setRole',
-                    data: {
-                        'gallery_id': i,
-                        'customer_id': customer_id,
-                        'role': 1
-                    },
-                    success: function (result) { }
-                });
-            }
-        }
-
-        // Change Autumn 1 Riddle Status From Unavailable To Accessible
-        for (var i = 72; i <= 76; i++) {
-            $.ajax({
-                type: 'POST',
-                url: '/api/setRole',
-                data: {
-                    'gallery_id': i,
-                    'customer_id': customer_id,
-                    'role': 1
-                },
-                success: function (result) { }
-            });
-        }
-
-        // Change Winter 1 Riddle Status From Unavailable To Accessible
-        for (var i = 78; i <= 79; i++) {
-            $.ajax({
-                type: 'POST',
-                url: '/api/setRole',
-                data: {
-                    'gallery_id': i,
-                    'customer_id': customer_id,
-                    'role': 1
+                    'role': 2
                 },
                 success: function (result) { }
             });
