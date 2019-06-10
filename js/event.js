fceb34d10a3e24149546e5f6a2d40b84aef2e320
silverpegasus <tuan.nm@appfast.io>
2019-06-10 09:42:24 +0700

    á

--- js/event.js
+++ js/event.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-	$('.button-group .button').click (function() {
+	$('.button-group .button').click(function () {
 		var btnID = $(this).attr('id');
 		if (btnID == 'start-btn')
 			window.location.href = '/all-riddles';
@@ -12,3 +12,40 @@ $(document).ready(function () {
 		$(this).closest('.modal').hide();
 	})
 });
+
+function postReview() {
+
+	var data = {};
+	data.rating = '10';
+	data.post_type = 'article';
+	data.post_id = '1';
+	data.title = $('input[name="level"]').val();
+	data.content = $('textarea[name="description"]').val();
+
+	if (!data.title) {
+		$('input[name="level"]').addClass('has-error');
+		toast('Vui lòng nhập tiêu đề mã', 3000, 'red');
+		return;
+	} else {
+		$('input[name="level"]').removeClass('has-error');
+	}
+
+	if (!data.content) {
+		$('textarea[name="description"]').addClass('has-error');
+		toast('Vui lòng nhập mô tả quá trình giải', 3000, 'red');
+		return;
+	} else {
+		$('textarea[name="description"]').removeClass('has-error');
+	}
+
+	StoreAPI.addReview(data, function (result) {
+		if (!result.code) {
+			toast('Gửi câu hỏi thành công!', 3000, 'blue');
+			setTimeout(function () {
+				location.reload();
+			}, 1000);
+		} else {
+			toast('Đã xảy ra lỗi!', 3000, 'red');
+		}
+	})
+}
